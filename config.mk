d0dbd1930d1bf260e5903a83a65014bc35abbe33
Akash Kollipara <akashkollipara@gmail.com>
2021-12-01 21:06:40 +0530

    First commit

---
+++ config.mk
@@ -0,0 +1,16 @@
+#
+# CYANCORE LICENSE
+# Copyrights (C) 2019, Cyancore Team
+#
+# File Name		: config.mk
+# Description		: This file consists of project config
+# Primary Author	: Akash Kollipara [akashkollipara@gmail.com]
+# Organisation		: Cyancore Core-Team
+#
+
+COMPILER	:= gcc
+FAMILY		:= mega_avr
+PLATFORM	:= atmega328p
+
+EARLYCON_SERIAL	:= 1		# Needed for boot message
+CONSOLE_SERIAL	:= 1		# Needed for printf after boot
---
+++ project.c
@@ -0,0 +1,46 @@
+/*
+ * CYANCORE LICENSE
+ * Copyrights (C) 2019, Cyancore Team
+ *
+ * File Name		: project.c
+ * Description		: This file consists of project srouces
+ * Primary Author	: Akash Kollipara [akashkollipara@gmail.com]
+ * Organisation		: Cyancore Core-Team
+ */
+
+#include <status.h>
+#include <arch.h>
+#include <stdio.h>
+#include <terravisor/bootstrap.h>
+#include <driver.h>
+#include "sort_with_rev/app.h"
+
+void plug()
+{
+	/* < ! > Plug in one time setup code */
+
+	/* Boot system */
+	bootstrap();
+
+	/* Setup/Start console */
+	driver_setup("console");
+
+	/* Print Banner */
+	printf("\n=================================================\n");
+	printf(" Example on Cyancore: Sort by reversing array!");
+	printf("\n=================================================\n");
+
+	/* Run the app */
+	app();
+	return;
+}
+
+void play()
+{
+	/* < ! > Play looping code here*/
+
+	/* Put CPU to sleep until interrupt is received */
+	/* This is a power saving feature */
+	arch_wfi();
+	return;
+}
